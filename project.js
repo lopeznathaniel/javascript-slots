3f20f56553e72559a4dca0749b36e8df8287483d
lopeznathaniel <119363758+lopeznathaniel@users.noreply.github.com>
2023-04-16 17:14:46 -0700

    Add the function for the user to deposit money

---
+++ project.js
@@ -0,0 +1,19 @@
+// -- START PROJECT SUMMARY --
+// 1. Deposit money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+// 7. Play again
+// -- END PROJECT SUMMARY --
+
+// Program asks user to input a number amount of money to deposit
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ")
+}
+
+deposit();
+
