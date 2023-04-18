8436e430a49c3444c5452b70f1d5ff440431d0d0
lopeznathaniel <119363758+lopeznathaniel@users.noreply.github.com>
2023-04-17 17:38:36 -0700

    Add the ability to reject and invalid input

--- project.js
+++ project.js
@@ -13,7 +13,12 @@ const prompt = require("prompt-sync")();
 
 const deposit = () => {
     const depositAmount = prompt("Enter a deposit amount: ")
-}
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+        console.log("Invalid deposit amount, try again.") 
+    }
+};
 
 deposit();
 
